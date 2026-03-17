ad88d5fb7038369128b16ee16897b71417541c28
Neksux <t53116656@gmail.com>
2026-03-17 23:30:12 +0600

    Create AimAssistScreen class in AimAssistScreen.java

---
+++ src/main/java/com/example/aimassist/AimAssistScreen.java
@@ -0,0 +1,13 @@
+package com.example.aimassist;
+
+public class AimAssistScreen {
+    // Class code for AimAssistScreen
+
+    public void display() {
+        System.out.println("Displaying Aim Assist Screen");
+    }
+
+    public void update() {
+        // Update logic for Aim Assist Screen
+    }
+}
