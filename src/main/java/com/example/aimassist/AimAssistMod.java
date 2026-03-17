634b5c86642bc34c305cc482a6422c1213feedb5
Neksux <t53116656@gmail.com>
2026-03-17 23:30:17 +0600

    Create AimAssistMod class implementing ClientModInitializer

---
+++ src/main/java/com/example/aimassist/AimAssistMod.java
@@ -0,0 +1,10 @@
+package com.example.aimassist;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AimAssistMod implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        // Client-side initialization code here
+    }
+}
